1ee329e69ba54ba1e6ec35705ea081d172e6cf37
ikhyunan <johnan0077@gmail.com>
2023-09-10 01:04:58 -0400

    two functioning modules - hello + greetings

--- hello/go.mod
+++ hello/go.mod
@@ -1,3 +1,7 @@
 module example/hello
 
 go 1.21.1
+
+replace example/greetings => ../greetings
+
+require example/greetings v1.1.0
--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+
+    "example/greetings"
+)
 
 func main() {
+    // Get greetings message from module: greetings
+    message := greetings.Hello("Ikhyun")
     fmt.Println("Hello, World!")
+    fmt.Println(message)
 }
